6ba1f188787333e641e9e0126e50a86c16ac8ccb
Lajos Seyler <lajosseyler@protonmail.com>
2024-11-14 17:39:21 +0100

    fix: URLs does not end with /

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -12,8 +12,8 @@ router.register("register", views.UserRegistrationViewSet, basename="register")
 urlpatterns = [
     path("", include(router.urls)),
     path("users/activate/<uuid>/<token>/", views.UserActivateView.as_view(), name="activate"),
-    path("token", views.TokenObtainPairView().as_view(), name="token_obtain_pair"),
-    path("token/refresh", views.TokenRefreshView().as_view(), name="token_refresh"),
+    path("token/", views.TokenObtainPairView().as_view(), name="token_obtain_pair"),
+    path("token/refresh/", views.TokenRefreshView().as_view(), name="token_refresh"),
     path("token/blacklist/", views.TokenBlacklistView.as_view(), name="token_blacklist"),
     path("token/verify/", TokenVerifyView.as_view(), name="token_verify"),
 ]
