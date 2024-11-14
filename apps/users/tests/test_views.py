b792b255fa13543243a726034cc75dad2a6f49c9
Lajos Seyler <lajosseyler@protonmail.com>
2024-11-14 19:42:35 +0100

    test: add tests for UserActivateView

--- apps/users/tests/test_views.py
+++ apps/users/tests/test_views.py
@@ -1,5 +1,10 @@
 import pytest
-from rest_framework.test import APIClient
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+from apps.users.factories import UserFactory
+
+User = get_user_model()
 
 
 @pytest.mark.django_db
@@ -10,8 +15,7 @@ from rest_framework.test import APIClient
         ["user2", "user2@example.com", "John", "Smith", "pass"],
     ],
 )
-def test_user_registration_view(username, email, first_name, last_name, password):
-    client = APIClient()
+def test_user_registration_view(drf_client, username, email, first_name, last_name, password):
     url = "/api/register/"
 
     data = {
@@ -22,7 +26,7 @@ def test_user_registration_view(username, email, first_name, last_name, password
         "password": password,
     }
 
-    response = client.post(url, data, format="json")
+    response = drf_client.post(url, data, format="json")
 
     assert response.status_code == 201
     assert response.data["username"] == username
@@ -37,10 +41,8 @@ def test_user_registration_view(username, email, first_name, last_name, password
 
 
 @pytest.mark.django_db
-def test_user_registration_view_invalid_data():
-    client = APIClient()
+def test_user_registration_view_invalid_data(drf_client):
     url = "/api/register/"
-
     data = {
         "username": "",
         "email": "",
@@ -49,7 +51,7 @@ def test_user_registration_view_invalid_data():
         "password": "",
     }
 
-    response = client.post(url, data, format="json")
+    response = drf_client.post(url, data, format="json")
 
     assert response.status_code == 400
     assert "username" in response.data
@@ -65,12 +67,72 @@ def test_user_registration_view_invalid_data():
 
 
 @pytest.mark.django_db
-def test_user_registration_view_method_not_allowed():
-    client = APIClient()
+def test_user_registration_view_method_not_allowed(drf_client):
     url = "/api/register/"
 
-    response = client.get(url)
+    response = drf_client.get(url)
 
     assert response.status_code == 405
     assert response.data == {"detail": 'Method "GET" not allowed.'}
     assert response["Allow"] == "POST"
+
+
+@pytest.mark.django_db
+def test_user_activate_view(drf_client):
+    register_url = "/api/register/"
+
+    user_data = UserFactory.build()
+
+    data = {
+        "username": user_data.username,
+        "email": user_data.email,
+        "first_name": user_data.first_name,
+        "last_name": user_data.last_name,
+        "password": user_data.password,
+    }
+
+    response = drf_client.post(register_url, data, format="json")
+    registered_user = User.objects.get(id=response.json()["id"])
+
+    assert registered_user.is_active is False
+
+    activate_url = reverse(
+        "apps.users:activate", kwargs={"uuid": registered_user.uuid, "token": registered_user.get_activation_token()}
+    )
+    response = drf_client.get(activate_url, follow=True)
+    registered_user.refresh_from_db()
+
+    assert response.status_code == 200
+    assert response.data["message"] == "User activated successfully."
+    assert registered_user.is_active is True
+
+
+@pytest.mark.django_db
+def test_user_activate_view_invalid_token(drf_client):
+    register_url = "/api/register/"
+
+    user_data = UserFactory.build()
+
+    data = {
+        "username": user_data.username,
+        "email": user_data.email,
+        "first_name": user_data.first_name,
+        "last_name": user_data.last_name,
+        "password": user_data.password,
+    }
+
+    response = drf_client.post(register_url, data, format="json")
+    registered_user = User.objects.get(id=response.json()["id"])
+
+    assert registered_user.is_active is False
+
+    activate_url = reverse(
+        "apps.users:activate",
+        kwargs={"uuid": registered_user.uuid, "token": "invalid_token"},
+    )
+    response = drf_client.get(activate_url, follow=True)
+    registered_user.refresh_from_db()
+
+    assert response.status_code == 400
+    assert response.data["message"] == "Invalid or expired activation token."
+    assert registered_user.is_active is False
